8b6ca9b31b31ee61f8dc6455161b037e8c31b0fb
Marco van Zoest <marco@primaldev.nl>
2015-01-29 15:29:48 +0100

    Initial

---
+++ MsiDumper/ParseMsi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInstaller;
+
+namespace MsiDumper
+{
+    class ParseMsi
+    {
+       
+        public ParseMsi(string msiFile)
+        {
+            Database database = getInstaller().OpenDatabase(msiFile, MsiOpenDatabaseMode.msiOpenDatabaseModeReadOnly);
+            parseDatabase(database);
+        }
+
+        public ParseMsi(string msiFile, string transForms)
+        {
+            Database database = getInstaller().OpenDatabase(msiFile, MsiOpenDatabaseMode.msiOpenDatabaseModeReadOnly);
+            foreach (String transForm in transForms.Split(new string[] { "," }, StringSplitOptions.None))
+            {
+                database.ApplyTransform(transForm, MsiTransformError.msiTransformErrorNone);
+            }
+
+            parseDatabase(database);
+        }
+
+
+        private Installer getInstaller()
+        {
+            Type type = Type.GetTypeFromProgID("WindowsInstaller.Installer");
+            return (Installer)Activator.CreateInstance(type);
+        }
+
+
+        private void parseDatabase(Database database)
+        {
+            WindowsInstaller.View view = null;
+            try
+            {
+                view = database.OpenView("SELECT * FROM `Property`");
+                view.Execute();
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("I made a boo boo, " + ex.Message);
+            }
+            
+
+            Record properties = view.Fetch();
+            while (properties !=null)
+            {
+                Console.WriteLine(properties.get_StringData(1));
+                properties = view.Fetch();
+            }
+
+           
+
+        }
+
+
+
+
+        
+    }
+}
---
+++ MsiDumper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInstaller;
+
+namespace MsiDumper
+{
+    class Program
+
+    {
+        
+        static void Main(string[] args)
+        {
+            new ParseMsi("d:\\temp\\Configuration_Manager_2012_R2_SDK_Setup.msi");
+
+        }
+
+
+       
+    }
+}
